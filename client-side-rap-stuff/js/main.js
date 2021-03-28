5070ef7966a383d7cffb71d43ad399f4541efe1a
regromrob <romae31@gmail.com>
2021-03-28 14:03:48 -0400

    api url location
    ls

--- client-side-rap-stuff/js/main.js
+++ client-side-rap-stuff/js/main.js
@@ -5,7 +5,7 @@ document.querySelector('button').addEventListener('click',getRapName)
 async function getRapName(){
     const rapName = document.querySelector('input').value
     try{
-    const res = await fetch(`http://localhost:8000/api/rappers/${rapName}`)
+    const res = await fetch(`https://rap-api-example.herokuapp.com/api/rappers/${rapName}`)
     const data = await res.json()
     console.log(data)
     document.querySelector('h2').innerText = data.birthName
